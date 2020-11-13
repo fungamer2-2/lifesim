137389c87a00013ef3f5d8bc01bbb108f46f94e5
Antimeuh2433 <justin.bax@icloud.com>
2020-11-13 17:25:00 -0500

    fixed random distribution

--- src/generateStat.cpp
+++ src/generateStat.cpp
@@ -3,21 +3,16 @@
 #include "../headers/generateStat.h"
 
 short int lsim::generateStat(float multiplier, short int bonus) {
-	// returns average of two random numbers to achieve a distribution somewhat centralized around 50-60
-	short int x = rand() % 100;
-	short int yRange = rand() % 10 + 15;
-	short int y = rand() % (100 - 2 * yRange) + yRange;
+	// returns average of two random numbers to achieve a distribution somewhat centralized around 60-70
+	short int x = rand() % 70 + 30;
+	short int y = rand() % 100;
 	short int result = (x + y) / 2 * multiplier;
+	short int distance = result - 60;
 	result += bonus;
 	if (result > 100) {
 		result = 100;
 	} else if (result < 0) {
 		result = 0;
 	}
-	short int offset = 0;
-	if ((x + y) / 2 * multiplier > 40 and (x + y) / 2 * multiplier < 60) {
-		offset = rand() % 14 - 7;
-	}
-	
-	return result + offset;
+	return result;a
 }
